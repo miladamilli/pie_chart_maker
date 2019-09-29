6e5734f1aa2f407d1a282587ad394c8071ce9e81
Milada Milli <milada.majak@gmail.com>
2019-09-29 17:24:15 +0200

    add route and link to Pie Chart

--- lib/live_view_demo_web/router.ex
+++ lib/live_view_demo_web/router.ex
@@ -19,6 +19,7 @@ defmodule LiveViewDemoWeb.Router do
 
     get "/", PageController, :index
     live "/clock", ClockLive
+    live "/piechart", PieChartLive
   end
 
   # Other scopes may use custom stacks.
